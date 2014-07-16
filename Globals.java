1c2484491175cf1c372692108adff770a43d4f3b
Ankita0123 <Ankita0123@users.noreply.github.com>
2014-07-16 07:45:09 +0530

    Create Globals.java

---
+++ Globals.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import android.content.Context;
+
+public class Globals
+{
+	public static Context context;
+	public static boolean isTest = false;
+	public static boolean isConnectionTest = false;
+	public static boolean isGZIP = false;
+	public static String applicationFolder = "genericApFolder";
+}
